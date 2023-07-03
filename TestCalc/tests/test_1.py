42e000b7bd0348db250635979b0d8bde5e874b26
ZhigarevOleg <111458739+ZhigarevOleg@users.noreply.github.com>
2023-07-03 14:39:45 +0300

    Add files via upload

---
+++ TestCalc/tests/test_1.py
@@ -0,0 +1,46 @@
+import pytest
+from app.calc import Calculator
+
+class TestCalc:
+    
+    def setup(self):
+        self.calc = Calculator
+        
+    #Позитивные тесты
+    
+    def test_multiply_success(self):
+        assert self.calc.multiply(self, 1, 2) == 2
+        
+    def test_division_success(self):
+        assert self.calc.division(self, 6, 3) == 2
+        
+    def test_subtraction_success(self):
+        assert self.calc.subtraction(self, 7, 4) == 3
+        
+    def test_adding_success(self):
+       assert self.calc.adding(self, 1, 1) == 2
+       
+    #Негативные тесты
+         
+    def test_multiply_unsuccess(self):
+        assert self.calc.multiply(self, 1, 2) != 3
+        
+    def test_adding_unsuccess(self):
+        assert self.calc.adding(self, 1, 1) != 5    
+    
+    def test_subtraction_unsuccess(self):
+        assert self.calc.subtraction(self, 7, 4) != 4
+    
+    def test_adding_unsuccess(self):
+       assert self.calc.adding(self, 1, 1) != 6
+           
+    def test_zero_division(self):
+        with pytest.raises(ZeroDivisionError):
+            self.calc.division(self, 1, 0)
+               
+    def teardown(self):
+        print ('Выполнение метода teardown')
+    
+    
+    
+        
